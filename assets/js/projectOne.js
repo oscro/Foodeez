17e76161b540115e392aa43da2b90e9ce0f2bd68
David Weid <david.weid.2@gmail.com>
2019-01-15 20:55:45 -0500

    added jquery ajax call

--- assets/js/projectOne.js
+++ assets/js/projectOne.js
@@ -1,3 +1,7 @@
 var apiKey = "w1wvrwmG8SQgOz5YXukdzsrC5FPM12g7haW9fRirPG141Gqv2YTbeqbm84u2vcZuMa3tSlZr2RAfc9hSnAzROIa5RsxdLSt41Dg7AXmcC931yBDRsgMSgvuAvIc-XHYx";
 
 var clientId = "70tPFL3C-kkqIjxx66gBdw";
+
+$.ajax({ url: queryURL, method: "GET"}).then(function(response){
+    //code
+})
