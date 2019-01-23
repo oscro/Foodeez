98b2c32be0ccfc65d97b5e5dbb46bdd57c244302
David Weid <david.weid.2@gmail.com>
2019-01-23 02:09:36 -0500

    updated index to reflect two different divs with two different search bars, one each for recipes and restaurants. Updated projectOne js to accept changes as well as edamam to reflect changes. Ajax calls via search bar currently work. Code cleaned up to an extent.

--- assets/js/edamam.js
+++ assets/js/edamam.js
@@ -1,38 +1,48 @@
-$(document).ready(function(){
-
-  console.log("EDAMAM!!!!!!!!!!!!!!!!");
-  
+$(document).ready(function() {
   var appId = "fb64e6f4";
-  
+
   var apiKey = "ca1d2e84cd055e2aea3d2b71186fa559";
 
-  
+  var recipe = "";
 
-  function displayObjectArry (object) {
+  $("#recipeClick").on("click", function() {
+    //Add code of when user clicks image
+  });
 
-      for(var i = 0; i <= object.hits[0].recipe.ingredients.length; i++){
-          
-          console.log(object.hits[i].recipe.ingredients[i].text);
-          
-      };
+  $(".recipe-item").on("click", function() {
+    $("#recipeSearchBar").val($(this).val());
+  });
 
-  };
-  
-  
-  $.ajax({
+  $("#recipeUserSubmit").on("click", function(e) {
+    event.preventDefault(e);
+    recipe = $("#recipeSearchBar").val();
+    console.log(recipe);
+
+    $.ajax({
       method: "GET",
-      url: "https://api.edamam.com/search?q=chicken&app_id=" + appId + "&app_key=" + apiKey,
+      url:
+        "https://api.edamam.com/search?q=" +
+        recipe +
+        "&app_id=" +
+        appId +
+        "&app_key=" +
+        apiKey,
       dataType: "json",
       success: function(result) {
-
         console.log(result);
 
         displayObjectArry(result);
-
       },
+
       error: function() {
         console.log("error");
       }
     });
-  
   });
+});
+
+function displayObjectArry(object) {
+  for (var i = 0; i < object.hits[0].recipe.ingredients.length; i++) {
+    console.log(object.hits[0].recipe.ingredients[i].text);
+  }
+};
